fc6a5a677446186dadf098181bc3fbeaf81cb123
TotalZero <radek.solekr@icloud.com>
2022-06-09 13:16:08 +0200

    little corrections in GameState

--- src/tools/GameState.ts
+++ src/tools/GameState.ts
@@ -1,7 +1,17 @@
 import Room from "../obj/Room";
 
+/**
+ * class stores information and game object
+ */
+
 export default class GameState {
      private static _rooms: Map<Function, Room> = new Map<Function, Room>()
+
+/**
+ * function creates and stores Room object
+ * @param fnc  class of the Room
+ * @returns  instance of class from @param fnc
+ */
 
     static getRoom(fnc: Function): Room {
         let room = GameState._rooms.get(fnc)
